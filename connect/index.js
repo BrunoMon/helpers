c4ae6bd6f85503a1305d96ebe92c3233e4769ed8
Bruno Monfrinotti <brunomon@hotmail.com>
2022-11-04 12:35:19 -0300

    mejoras en connect

--- connect/index.js
+++ connect/index.js
@@ -1,47 +1,53 @@
-import {
-    deepValue
-} from "../deepValue"
+/** @format */
 
-export const connect = (store, ...paths) => (baseElement) => class extends baseElement {
-    constructor() {
-        super();
-        this.__currentStates = []
-        this.__deepValues = []
-    }
-    connectedCallback() {
-        for (let i = 0; i < paths.length; i++) {
-            const path = paths[i].split(".")
-            this.__currentStates.push(null)
-            let deepVal = path.length > 0 ? deepValue(...path) : null;
-            this.__deepValues.push(deepVal)
-            this.__storeUnsubscribe = store.subscribe(() => this.__stateChanged(i, store.getState()));
-            this.__stateChanged(i, store.getState());
-        }
-        if (super.connectedCallback) {
-            super.connectedCallback();
-        }
-    }
-    getCurrent() {
-        return [...this.__currentStates]
-    }
-    disconnectedCallback() {
-        this.__storeUnsubscribe();
-        if (super.disconnectedCallback) {
-            super.disconnectedCallback();
-        }
-    }
+import { deepValue } from "../deepValue";
 
-    __stateChanged(i, state) {
-        if (this.stateChanged) {
-            let change = true
-            if (this.__deepValues[i]) {
-                let previousState = this.__currentStates[i]
-                this.__currentStates[i] = this.__deepValues[i](state)
-                change = previousState != this.__currentStates[i]
+export const connect =
+    (store, ...paths) =>
+    (baseElement) =>
+        class extends baseElement {
+            constructor() {
+                super();
+                this.store = store;
+                this.__currentStates = [];
+                this.__deepValues = [];
             }
-            if (change) {
-                this.stateChanged(state, paths[i])
+
+            connectedCallback() {
+                for (let i = 0; i < paths.length; i++) {
+                    const path = paths[i].split(".");
+                    this.__currentStates.push(null);
+                    let deepVal = path.length > 0 ? deepValue(...path) : null;
+                    this.__deepValues.push(deepVal);
+                    this.__currentStates[i] = this.__deepValues[i](store.getState());
+                    this.__storeUnsubscribe = store.subscribe(() => this.__stateChanged(i, store.getState()));
+                    this.__stateChanged(i, store.getState());
+                }
+                if (super.connectedCallback) {
+                    super.connectedCallback();
+                }
             }
-        }
-    }
-};
+            getCurrent() {
+                return [...this.__currentStates];
+            }
+            disconnectedCallback() {
+                this.__storeUnsubscribe();
+                if (super.disconnectedCallback) {
+                    super.disconnectedCallback();
+                }
+            }
+
+            __stateChanged(i, state) {
+                if (this.stateChanged) {
+                    let change = true;
+                    if (this.__deepValues[i]) {
+                        let previousState = this.__currentStates[i];
+                        this.__currentStates[i] = this.__deepValues[i](state);
+                        change = previousState != this.__currentStates[i];
+                    }
+                    if (change) {
+                        this.stateChanged(state, paths[i]);
+                    }
+                }
+            }
+        };
